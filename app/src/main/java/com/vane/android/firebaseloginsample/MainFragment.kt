71c7d01c43ad2868e277a90c0bf045e666bcbaf6
ivVane <iv.vane@gmail.com>
2021-09-05 19:27:53 +0200

    Added project dependecies and created needed xml files

---
+++ app/src/main/java/com/vane/android/firebaseloginsample/MainFragment.kt
@@ -0,0 +1,25 @@
+package com.vane.android.firebaseloginsample
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.vane.android.firebaseloginsample.databinding.FragmentMainBinding
+
+class MainFragment : Fragment() {
+
+    private lateinit var binding: FragmentMainBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_main, container, false)
+
+        return binding.root
+    }
+
+}
